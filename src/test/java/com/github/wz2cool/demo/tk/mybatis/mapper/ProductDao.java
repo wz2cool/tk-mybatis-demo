52cef47785b646f8ac4a3640a6bb9825ba66d416
wz2cool <wz2cool@live.cn>
2017-08-04 14:18:46 +0800

    add

---
+++ src/test/java/com/github/wz2cool/demo/tk/mybatis/mapper/ProductDao.java
@@ -0,0 +1,21 @@
+package com.github.wz2cool.demo.tk.mybatis.mapper;
+
+import com.github.wz2cool.demo.tk.mybatis.model.entity.table.Product;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: Frank
+ * \* Date: 8/4/2017
+ * \* Time: 2:12 PM
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+@Mapper
+public interface ProductDao {
+
+    List<Product> getProducts();
+}
