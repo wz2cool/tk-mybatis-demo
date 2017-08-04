7af905acd27b6c89ed241ed7305d1a90b01f1b79
wz2cool <wz2cool@live.cn>
2017-08-04 14:00:56 +0800

    add

---
+++ src/test/java/com/github/wz2cool/demo/tk/mybatis/TestApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.demo.tk.mybatis;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: Frank
+ * \* Date: 8/4/2017
+ * \* Time: 2:00 PM
+ * \* To change this template use File | Settings | File Templates.
+ * \* Description:
+ * \
+ */
+public class TestApplication {
+}
